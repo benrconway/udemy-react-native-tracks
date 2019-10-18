f85e35b7d7b112704c39840062c38c270e20b1b2
Ben Conway <benrconway84@gmail.com>
2019-10-18 15:51:32 +0100

    Boilerplate code added for six screens

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const SignupScreen = () => {
+  return (
+    <Text style={{ fontSize: 40 }}>SignupScreen</Text>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SignupScreen;
