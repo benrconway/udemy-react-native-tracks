6570785c7948639f19de783943a162cf3c7e59d7
Ben Conway <benrconway84@gmail.com>
2019-10-21 11:55:30 +0100

    Added signup pathway into application

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,21 +1,30 @@
+import { AsyncStorage } from 'react-native';
 import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
+import tracker from '../api/tracker';
+import { navigate } from '../navigationRef';
 
 const authReducer = (state, action) => {
   switch(action.type){
+    case 'add_error':
+      return { ...state, errorMessage: action.payload }
+    case 'singup':
+      return { errorMessage: '', token: action.payload, }
     default:
       return state;
     }
 }
 
-const signup = dispatch => {
-  return ({ email, password }) => {
-    // make api request to sign up with email and password
-
-    // if we succeed, modify state to show we are authenticated
-
-    // display error if there is an issue signing-up
-  }
-}
+const signup = dispatch => async ({ email, password }) => {
+  try {
+    const response = await trackerApi.post('/signup', { email, password });
+    await AsyncStorage.setItem('token', response.data.token);
+    dispatch({ type: 'signup', payload: response.data.token });
+    navigate('TrackList')
+  } catch (err) {
+    dispatch({ type: 'add_error' , payload: 'Something went wrong with sign up'});
+  };
+};
 
 const signin = dispatch => {
   return ({ email, password }) => {
@@ -40,5 +49,8 @@ export const { Provider, Context } = createDataContext(
     signin,
     signout
   },
-  { isSignedIn: false }
+  {
+    token: null,
+    errorMessage: '',
+  }
 )
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
+import { Context as AuthContext } from '../context/AuthContext';
 
 const SignupScreen = ({ navigation }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  const { state, signup } = useContext(AuthContext);
 
   return (
     <View style={styles.container}>
@@ -29,8 +30,13 @@ const SignupScreen = ({ navigation }) => {
         onChangeText={setPassword}
         secureTextEntry
       />
+      { state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null }
       <Spacer />
-      <Button title="Sign up"/>
+      <Button title="Sign up"
+        onPress={() => signup({ email, password }, () => {
+          navigation.navigate('mainFlow');
+        })}
+      />
     </View>
   );
 };
@@ -46,6 +52,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     marginBottom: 120
+  },
+  errorMessage: {
+    fontSize: 18,
+    color: 'red',
+    marginLeft: 15,
+    marginTop: 15,
   }
 });
 
