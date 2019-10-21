0123aabdab09c1a1514feb71b1422790a33b2339
Ben Conway <benrconway84@gmail.com>
2019-10-21 14:25:33 +0100

    added fake location change code

--- src/components/Map.js
+++ src/components/Map.js
@@ -4,12 +4,6 @@ import MapView, { Polyline } from 'react-native-maps';
 
 const Map = () => {
   let points = [];
-  for (let i = 0; i < 20; i++) {
-    points.push({
-      latitude: 37.33233 + i * 0.001,
-      longitude: -122.03121 + i * 0.001
-    })
-  }
 
   return (
     <MapView
@@ -30,6 +24,6 @@ const styles = StyleSheet.create({
   map: {
     height: 300
   }
-});
+})
 
 export default Map;
